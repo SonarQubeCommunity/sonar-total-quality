ae7c7e329136df2a4fa2a71f2809d106b24bfd9e
Martin Martin <e72636@gmail.com>
2010-03-17 15:02:04 +0000

    more tests

--- src/test/java/org/sonar/plugins/totalquality/TestDecoratorTest.java
+++ src/test/java/org/sonar/plugins/totalquality/TestDecoratorTest.java
@@ -27,77 +27,52 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.sonar.api.batch.DecoratorContext;
 import org.sonar.api.measures.CoreMetrics;
 import org.sonar.api.measures.Measure;
 import org.sonar.api.resources.Resource;
 
-
-public class DrynessDecoratorTest {
+public class TestDecoratorTest {
 
   @Test
-  public void testDecorate() {
-    final Measure dup = mock(Measure.class);
-    when(dup.getValue()).thenReturn(45D);
-
-    final Measure ncl = mock(Measure.class);
-    when(ncl.getValue()).thenReturn(4500D);
+  public void testDecorator() {
+    final double cov = 45D;
+    final double suc = 80D;
+    
+    final double val = (0.80 * cov) + (0.20 * suc); 
     
     final Resource res = mock(Resource.class);
     when(res.getQualifier()).thenReturn(Resource.QUALIFIER_CLASS);
     
+    final Measure ncl = mock(Measure.class);
+    when(ncl.getValue()).thenReturn(4500D);
+
+    final Measure mcov = mock(Measure.class);
+    when(mcov.getValue()).thenReturn(cov);
+
+    final Measure msuc = mock(Measure.class);
+    when(msuc.getValue()).thenReturn(suc);
+
+    
     final DecoratorContextSupport context = spy(new DecoratorContextSupport());
-    
-    when(context.getMeasure(CoreMetrics.DUPLICATED_LINES_DENSITY)).thenReturn(dup);
     when(context.getMeasure(CoreMetrics.NCLOC)).thenReturn(ncl);
+    when(context.getMeasure(CoreMetrics.TEST_SUCCESS_DENSITY)).thenReturn(msuc);
+    when(context.getMeasure(CoreMetrics.COVERAGE)).thenReturn(mcov);
     
-    final DrynessDecorator decorator = new DrynessDecorator();
+    
+    final TestDecorator decorator = new TestDecorator() {
+      @Override
+      protected String getLine(DecoratorContext context) {
+        return TQPlugin.TQ_TEST_FORMULA_DEFAULT;
+      }
+    };
     
     decorator.decorate(res, context);
     
     assertNotNull(context.getD());
-    assertTrue(55D == context.getD());
+    assertTrue(val == context.getD());
+    
   }
   
-  @Test
-  public void testNoDecorateNoLines() {
-    final Measure dup = mock(Measure.class);
-    when(dup.getValue()).thenReturn(45D);
-
-    final Resource res = mock(Resource.class);
-    when(res.getQualifier()).thenReturn(Resource.QUALIFIER_CLASS);
-    
-    final DecoratorContextSupport context = spy(new DecoratorContextSupport());
-    
-    when(context.getMeasure(CoreMetrics.DUPLICATED_LINES_DENSITY)).thenReturn(dup);
-    
-    final DrynessDecorator decorator = new DrynessDecorator();
-    
-    decorator.decorate(res, context);
-    
-    assertNull(context.getD());
-  }
-
-  @Test
-  public void testNoDecorateTestClass() {
-    final Measure dup = mock(Measure.class);
-    when(dup.getValue()).thenReturn(45D);
-
-    final Measure ncl = mock(Measure.class);
-    when(ncl.getValue()).thenReturn(4500D);
-    
-    final Resource res = mock(Resource.class);
-    when(res.getQualifier()).thenReturn(Resource.QUALIFIER_UNIT_TEST_CLASS);
-    
-    final DecoratorContextSupport context = spy(new DecoratorContextSupport());
-    
-    when(context.getMeasure(CoreMetrics.DUPLICATED_LINES_DENSITY)).thenReturn(dup);
-    when(context.getMeasure(CoreMetrics.NCLOC)).thenReturn(ncl);
-    
-    final DrynessDecorator decorator = new DrynessDecorator();
-    
-    decorator.decorate(res, context);
-    
-    assertNull(context.getD());
-  }
   
 }
